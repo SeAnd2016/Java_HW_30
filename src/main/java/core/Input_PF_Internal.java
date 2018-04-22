1278fc7a235eb58012636c720e66ec1204bbd507
Sergei Andreev <sergei.andreev.us@gmail.com>
2018-04-21 20:52:31 -0700

    First commit

---
+++ src/main/java/core/Input_PF_Internal.java
@@ -0,0 +1,19 @@
+package core;
+
+import java.io.*;
+import java.util.*;
+
+public class Input_PF_Internal {
+	public static void main(String[] args) throws IOException {
+		
+		Properties p = new Properties();
+		p.load(ClassLoader.getSystemResourceAsStream("input.properties"));
+		
+		String first_name = p.getProperty("f_name");
+		String last_name = p.getProperty("l_name");
+		System.out.println("Full name is: " + first_name + " " + last_name);
+		
+		p.clear();
+		
+	}
+}
